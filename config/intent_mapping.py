699d1e7deb8c1463ea93d8ae77c2554d4987aa9f
Charlie McCormick-Barnes <151023884+chmcbs@users.noreply.github.com>
2025-10-18 16:52:30 +0100

    feat: add intent mapping configuration

---
+++ config/intent_mapping.py
@@ -0,0 +1,34 @@
+"""
+Intent mapping configuration for entity extraction
+
+"""
+
+import pandas as pd
+
+def get_intent_mapping(trees_df, patches_df):
+    return {
+        'tree_recommendations': {
+            'column_to_extract': trees_df['level_requirement'],
+            'column_to_return': trees_df['tree_name']
+        },
+        'level_requirements': {
+            'column_to_extract': trees_df['tree_name'],
+            'column_to_return': trees_df['level_requirement']
+        },
+        'quest_requirements': {
+            'column_to_extract': patches_df['location_simple'],
+            'column_to_return': patches_df['patch_requirement']
+        },
+        'payment': {
+            'column_to_extract': trees_df['tree_name'],
+            'column_to_return': trees_df['payment_name']
+        },
+        'growth_time': {
+            'column_to_extract': trees_df['tree_name'],
+            'column_to_return': trees_df['growth_time_minutes']
+        },
+        'transportation': {
+            'column_to_extract': patches_df['location_simple'],
+            'column_to_return': patches_df['transportation_methods']
+        }
+    }
